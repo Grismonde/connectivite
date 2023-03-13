ffe817b25b7676c4cf80b903ef20d602995c04e6
Grismonde <verdelhan.francois@gmail.com>
2023-03-13 14:18:23 +0100

    Evolution

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var app = express();
 
 const port = 3000;
 
+app.get("/toto", function(req,res){ res.send("Salut toto")} );
+
+
 app.listen(port, function(){
-    console.log('Serveur plop')
+    console.log('Serveur listing on port : ' + port);
 });
