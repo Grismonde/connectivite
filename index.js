bf409e68958d678c7c90584588dcac237abd5e8a
Grismonde <verdelhan.francois@gmail.com>
2023-03-13 15:33:30 +0100

    Evolution

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-/* var express = require('express');
+var express = require('express');
 var app = express();
 
 const port = process.env.PORT || 3000;
@@ -8,33 +8,4 @@ app.get("/toto", function(req,res){ res.send("Salut toto")} );
 
 app.listen(port, function(){
     console.log('Serveur listing on port : ' + port);
-}); */
-
-const express = require('express');
-const app = express();
-const request = require('request');
-const cheerio = require('cheerio');
-const port = process.env.PORT || 3000;
-
-app.use(express.static('public'));
-
-app.get('/metadata', (req, res) => {
-  const url = req.query.url;
-  request(url, (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-      const $ = cheerio.load(html);
-      const metadata = {
-        title: $('meta[property="og:title"]').attr('content'),
-        description: $('meta[property="og:description"]').attr('content'),
-        image: $('meta[property="og:image"]').attr('content')
-      };
-      res.send(metadata);
-    } else {
-      res.status(400).send('URL invalide');
-    }
-  });
-});
-
-app.listen(port, () => {
-  console.log(`Le serveur est en écoute sur le port ${port}`);
 });
